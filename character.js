8317f51eb3914cf0f7e3f7ecf7a9f9dafddf8bc0
itsrhena <mecharhenalyn@gmail.com>
2025-11-27 19:21:16 +0800

    first commit

---
+++ character.js
@@ -0,0 +1,86 @@
+var character = [
+  {
+    "name": "Atsushi Nakajima",
+    "ability": "Beast Beneath the Moonlight",
+    "organization": "Armed Detective Agency",
+    "badge": "badge-ada",
+    "picture": "Nakajima.jpg"
+  },
+  {
+    "name": "Osamu Dazai",
+    "ability": "No Longer Human",
+    "organization": "Armed Detective Agency",
+    "badge": "badge-ada",
+    "picture": "Dazai.jpg"
+  },
+  {
+    "name": "Chūya Nakahara",
+    "ability": "For the Tainted Sorrow",
+    "organization": "Port Mafia",
+    "badge": "badge-port",
+    "picture": "Nakahara.jpg"
+  },
+   {
+    "name": "Ryūnosuke Akutagawa",
+    "ability": "Rashōmon",
+    "organization": "Port Mafia",
+    "badge": "badge-port",
+    "picture": "Akutagawa.jpg"
+  }, 
+  {
+    "name": "Doppo Kunikida",
+    "ability": "Doppo Poet",
+    "organization": "Armed Detective Agency",
+    "badge": "badge-ada",
+    "picture": "Kunikida.jpg"
+  },
+   {
+    "name": "Fyodor Dostoevsky",
+    "ability": "Crime and Punishment",
+    "organization": "The Decay of Angel",
+    "badge": "badge-rats",
+    "picture": "Dostoevsky.jpg"
+  },
+   {
+    "name": "Edogawa Ranpo",
+    "ability": "Super Deduction",
+    "organization": "Armed Detective Agency",
+    "badge": "badge-ada", 
+    "picture": "Edogawa.jpg"
+  },
+   {
+    "name": "Kyouka Izumi",
+    "ability": "Demon Snow",
+    "organization": "Port Mafia",
+    "badge": "badge-port", 
+    "picture": "Izumi.jpg"
+  },
+   {
+    "name": "Akiko Yosano",
+    "ability": "Thou Shalt Not Die",
+    "organization": "Armed Detective Agency",
+    "badge": "badge-ada",
+    "picture": "Yosano.jpg"
+  },
+   {
+    "name": "Yumeno Kyusaku (Q)",
+    "ability": "Dogra Magra",
+    "organization": "Port Mafia",
+    "badge": "badge-port",
+    "picture": "Kyusaku.jpg"
+  },
+   {
+    "name": "Kenji Miyazawa",
+    "ability": "Undefeated by the Rain",
+    "organization": "Armed Detective Agency",
+    "badge": "badge-ada",
+    "picture": "Miyazawa.jpg"
+  },
+   {
+    "name": "Nikolai Gogol",
+    "ability": "The Overcoat",
+    "organization": "Decay of the Angel",
+    "badge": "badge-rats",
+    "picture": "Gogol.jpg"
+  }
+];
